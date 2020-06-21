adcbebb1ddd12f786b42232e4083ebfa397f2781
Fahad Mustafa <fahad-2221@hotmail.co.uk>
2020-06-21 14:44:34 +0100

    fixed database bug

--- application.py
+++ application.py
@@ -1,30 +1,38 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, session
+from flask import Flask, request, render_template
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    title = "Welcome to the book store"
-    return render_template("index.html", title = title,)
+    title = "hi"
+    return render_template("index.html",title=title)
 
-@app.route("/hello", methods=["GET","POST"])
-def hello():
-    if request.method == "GET":
-        return "Please submit the form instead."
-    else:
-        name = request.form.get("name");
-        return render_template("hello.html", name = name)
-
-#@app.route("/notepad", methods=["GET", "POST"])
-#def notepad():
-#    if "notes" in session:
-#        session["notes"] = []
-#    if request.method == "POST":
-#        note = request.form.get("note")
-#        session["notes"].append(note)
-#    return render_template("notepad.html", notes = session["notes"])
-
-#@app.route("/<string:name>")
-#def welcome(name):
-#    name = name.capitalize();
-#    return f"Hi, {name}!"
+@app.route("/notepad", methods=["GET","POST"])
+def notepad():
+    
+    if request.method =="POST":
+        note = request.form.get("note")
+        session["notes"].append(note)
+    if session.get("notes") is None:
+        session["notes"]=[]
+    return render_template("notepad.html",notes=session["notes"])
